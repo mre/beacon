47ed77961524eb493767327e893b178689e17e3a
Matthias Endler <matthias.endler@trivago.com>
2014-11-13 16:08:25 +0100

    Add config file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+  'statsd' => [
+    'namespace' => 'beacon',
+    'escape_char' => '_',
+    'host' => '127.0.0.1',
+    'port' => 8125
+  ]
+];
+
--- src/mre/Beacon.php
+++ src/mre/Beacon.php
@@ -2,11 +2,14 @@
 
 namespace mre;
 
+use \Noodlehaus\Config as Config;
+
 class Beacon
 {
 	public function __construct()
 	{
-		file_put_contents("metrics.txt", json_encode(array_merge($_GET, $_SERVER)));
+		//file_put_contents("metrics.txt", json_encode(array_merge($_GET, $_SERVER)));
+		file_put_contents("metrics.txt", "teste");
 	}
 }
 
