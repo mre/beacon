2a7c756d0cb8e5714132d8d3b375ea2aaa68c34b
Matthias Endler <matthias.endler@trivago.com>
2014-11-13 14:30:52 +0100

    Composerize

--- beacon.php
+++
@@ -1,4 +0,0 @@
-<?php
-  header("HTTP/1.0 204 No Content");
-  file_put_contents("metrics.txt", json_encode(array_merge($_GET, $_SERVER)));
-?>
---
+++ src/mre/beacon.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace mre;
+
+header("HTTP/1.0 204 No Content");
+file_put_contents("metrics.txt", json_encode(array_merge($_GET, $_SERVER)));
+
+?>
