20faf1b64126cc21b051f6231b89e78b37bdc743
Matthias Endler <matthias.endler@trivago.com>
2015-02-09 13:45:36 +0100

    Add security check for requests

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
 header('HTTP/1.0 204 No Content');
 
-require_once __DIR__ . '/vendor/autoload.php';
-
-$beacon = new \mre\Beacon('config.php');
-$beacon->sendMetrics();
+if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest')
+{
+    require_once __DIR__ . '/vendor/autoload.php';
+    $beacon = new \mre\Beacon('config.php');
+    $beacon->sendMetrics();
+}
