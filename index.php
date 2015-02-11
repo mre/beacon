de5fad270d6b3a6061c8a29338f21c30224af18e
janvt <janvt@me.com>
2015-02-11 11:37:03 +0100

    set headers to prevent caching

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 header('HTTP/1.0 200 OK');
 
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest')
