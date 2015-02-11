a66d77dc5373f92ac6d6197d0cbe9e2a7d2e2e73
Matthias Endler <matthias-endler@gmx.net>
2015-02-11 13:16:24 +0100

    Use github's inline code editor to break and fix the build

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 header('HTTP/1.0 200 OK');
-header("Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate");
-header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
-header('Last-Modified: {now} GMT);
+header('Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate');
+header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: {now} GMT');
 
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest')
 {
