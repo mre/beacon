ac2067e9d2c2d4ca2ba13aa4ce2460f9693015c9
Matthias Endler <matthias.endler@trivago.com>
2015-02-12 13:40:37 +0100

    Use correct namespace for beacon backend

--- index.php
+++ index.php
@@ -5,9 +5,11 @@ header('Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate');
 header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
 header('Last-Modified: {now} GMT');
 
+use mre\beacon\Beacon;
+
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest')
 {
     require_once __DIR__ . '/vendor/autoload.php';
-    $beacon = new \mre\Beacon('config.php');
+    $beacon = new Beacon('config.php');
     $beacon->sendMetrics();
 }
