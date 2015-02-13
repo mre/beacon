9c8bdedf87dee2f67222f3d141b53688853b5f9a
Matthias Endler <matthias.endler@trivago.com>
2015-02-13 16:27:43 +0100

    Remove unused allowed keys in config

--- config.php
+++ config.php
@@ -2,32 +2,12 @@
 return [
     'statsd' => [
         'namespace' => 'rum',
-        'escape_char' => '_',
         'host' => '127.0.0.1',
         'port' => 8125,
         'timeout' => 2 // Connection timeout in seconds
     ],
-    'send_keys' => [
-        // For more info see
-        // https://developer.mozilla.org/en-US/docs/Navigation_timing
-        'nt_con', // Connection
-        'nt_dns', // DNS lookup
-        'nt_domcomp', // DOM complete
-        'nt_domcontloaded', // DOM content loaded
-        'nt_domint', // DOM interactive
-        'nt_domloading', // DOM loading
-        'nt_fet', // Fetch
-        'nt_load', // LoadEvent
-        'nt_nav', // Navigation start
-        'nt_nav_type', // Navigation type
-        'nt_red_cnt', // Redirect count
-        'nt_red', // Redirect
-        'nt_req', // Request
-        'nt_res', // First byte time
-        'nt_unload', // Unload event
-        'u', // Original requested url
-        'v', // Boomerang version
-        // Add optional custom parameters here
-        'page_id'
+    'allowed_keys' => [
+        // A comma separated list of allowed keys (as strings)
+        // All other keys will be ignored
     ]
 ];
