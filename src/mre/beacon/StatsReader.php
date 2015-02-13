870b99f3fbdef1c13301673621989466793b9d66
Matthias Endler <matthias.endler@trivago.com>
2015-02-13 09:31:12 +0100

    Remove unused code

--- src/mre/beacon/StatsReader.php
+++ src/mre/beacon/StatsReader.php
@@ -49,28 +49,6 @@ class StatsReader
     }
 
     /**
-     * Create a new metric point
-     *
-     * @param $_sKey string The metric key
-     * @param $_sPoint string The metric value with type
-     * @return Metric|null
-     */
-    private function createMetric($_sKey, $_sPoint)
-    {
-        $_aValueAndType = $this->oValidator->splitValueAndType($_sPoint);
-
-        if (count($_aValueAndType) != 3)
-        {
-            return null;
-        }
-
-        $_iValue = $_aValueAndType[1];
-        $_sType = $_aValueAndType[2];
-
-        return new Metric($_sKey, $_iValue, $_sType);
-    }
-
-    /**
      * Return all valid metrics in the data array
      *
      * @param array $aData Data array
@@ -92,5 +70,21 @@ class StatsReader
             }
         }
         return $_aMetrics;
+    }
+
+    /**
+     * Create a new metric point
+     *
+     * @param $_sKey string The metric key
+     * @param $_sPoint string The metric value with type
+     * @return Metric|null
+     */
+    private function createMetric($_sKey, $_sPoint)
+    {
+        $_aValueAndType = $this->oValidator->splitValueAndType($_sPoint);
+        $_iValue = $_aValueAndType[1];
+        $_sType = $_aValueAndType[2];
+
+        return new Metric($_sKey, $_iValue, $_sType);
     }
 }
