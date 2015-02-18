b44c03984c6a385e7c3a4605191bb2e106d5095d
Matthias Endler <matthias.endler@trivago.com>
2015-02-18 22:40:55 +0100

    Add dependency injection to bootstrap step

--- bootstrap.php
+++ bootstrap.php
@@ -1,14 +1,32 @@
 <?php
 
+namespace mre\Beacon;
+/*
 if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || !strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest')
 {
     // Stop if not ajax request
     // to prevent unwanted calls
     return;
 }
+*/
+use Auryn\Provider;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$_aConfig = \mre\Beacon\ConfigLoader::load('config.php');
-$_oBeacon = new mre\Beacon\Beacon($_aConfig);
-$_oBeacon->run();
+$aConfig = ConfigLoader::load('config.php');
+
+$oInjector = new Provider;
+
+$oInjector->define('Domnikl\Statsd\Connection\InetSocket', [
+    ':host' => $aConfig['statsd']['host'],
+    ':port' => $aConfig['statsd']['port'],
+    ':timeout' => $aConfig['statsd']['timeout']
+]);
+
+$oInjector->define('Domnikl\Statsd\Client', [
+    'connection' => 'Domnikl\Statsd\Connection\UdpSocket',
+    ':namespace' => $aConfig['statsd']['namespace']
+]);
+
+$_oBeacon = $oInjector->make('mre\Beacon\Beacon');
+$_oBeacon->run(filter_input_array(INPUT_GET));
