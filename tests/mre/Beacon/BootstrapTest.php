e9903fc2894c76df0c5fc5c32e2f4a9f704cec35
Matthias Endler <matthias.endler@trivago.com>
2015-02-25 00:09:28 +0100

    Improve code coverage

--- tests/mre/Beacon/BootstrapTest.php
+++ tests/mre/Beacon/BootstrapTest.php
@@ -27,4 +27,36 @@ class BootstrapTest extends \PHPUnit_Framework_TestCase
         $_sExpectedNamespace = $this->aConfig['statsd']['namespace'] . '.my.app';
         $this->assertEquals($_sExpectedNamespace, $_oBootstrap->getNamespace());
     }
+
+    public function testGetSetNamespace()
+    {
+        $_oBootstrap = new Bootstrap($this->aConfig, ['REQUEST_URI' => '/beacon/my/app'], ['foo' => '123c']);
+        $_sTestNamespace = 'my.namespace';
+        $_oBootstrap->setNamespace($_sTestNamespace);
+        $this->assertEquals($_sTestNamespace, $_oBootstrap->getNamespace());
+    }
+
+    public function testGetSetConfig()
+    {
+        $_oBootstrap = new Bootstrap($this->aConfig, ['REQUEST_URI' => '/beacon/my/app'], ['foo' => '123c']);
+        $_aConfig = [];
+        $_oBootstrap->setConfig($_aConfig);
+        $this->assertEquals($_aConfig, $_oBootstrap->getConfig());
+    }
+
+    public function testGetSetServerEnv()
+    {
+        $_oBootstrap = new Bootstrap($this->aConfig, ['REQUEST_URI' => '/beacon/my/app'], ['foo' => '123c']);
+        $_aServerEnv= [];
+        $_oBootstrap->setServerEnv($_aServerEnv);
+        $this->assertEquals($_aServerEnv, $_oBootstrap->getServerEnv());
+    }
+
+    public function testGetSetMetricData()
+    {
+        $_oBootstrap = new Bootstrap($this->aConfig, ['REQUEST_URI' => '/beacon/my/app'], ['foo' => '123c']);
+        $_aMetricData = [];
+        $_oBootstrap->setMetricData($_aMetricData);
+        $this->assertEquals($_aMetricData, $_oBootstrap->getMetricData());
+    }
 }
